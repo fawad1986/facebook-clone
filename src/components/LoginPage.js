d5943227b5d4fa9d613fa36fc3b99497846db182
Fawad Hassan <syedfawadhassan86@gmail.com>
2021-08-23 06:55:25 +0500

    first commit

---
+++ src/components/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LoginPage() {
+return(
+    
+)
+}
+
+
+
+
+export default LoginPage;
---
+++ src/components/Timeline.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Timeline() {
+return(
+    
+)
+}
+
+
+
+
+export default Timeline;
